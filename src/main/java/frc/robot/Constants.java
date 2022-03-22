6217a169ec348391dab8dbd66192e0960af73458
Aiden <100034083+AidenTheKarmic@users.noreply.github.com>
2022-03-22 19:24:33 -0400

    Added secret weapon
    AC
    
    - added compressor to secret weapon.java
    - driver left bumper activates secret weapon
    - driver right bumper deactivates secret weapon

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -10,7 +10,8 @@ public class Constants {
     public static final int k_catapult = 6;
     public static final int k_catapultSwitch = 0;
     public static final int k_intake = 2;
-    public static final int k_swSolenoid = 0;
+    public static final int k_swForward = 0;
+    public static final int k_swReverse = 1; 
     public static final int k_climb = 7;
 
 
--- src/main/java/frc/robot/SecretWeapon.java
+++ src/main/java/frc/robot/SecretWeapon.java
@@ -3,13 +3,16 @@ package frc.robot;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+import edu.wpi.first.wpilibj.Compressor;
 
 public class SecretWeapon {
 
     DoubleSolenoid doubleSolenoid;
-
-    public void k_swSolenoid (int forward, int back) {
+    Compressor compressor;
+ 
+    public SecretWeapon (int forward, int back) {
         doubleSolenoid = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, forward, back);
+        compressor = new Compressor(PneumaticsModuleType.CTREPCM);
     }
 
     public void activate(){
